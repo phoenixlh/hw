f4517c4cf62352b045df1752820f8e2823fcf30b
lihong <1224954159@qq.com>
2018-01-24 09:17:14 +0800

    dfdfd

---
+++ js/js.js
@@ -0,0 +1,13 @@
+//lunbo
+//<div class="lb">
+//				<ul class="xlb">
+//					<li><a><img src="img/datu1.jpg"></a></li>
+//					<li><a><img src="img/datu2.jpg"></a></li>
+//					<li><a><img src="img/datu3.jpg"></a></li>
+//					<li><a><img src="img/datu4.jpg"></a></li>
+//					<li><a><img src="img/datu1.jpg"></a></li>
+//					<li><a><img src="img/datu2.jpg"></a></li>
+//					<li><a><img src="img/datu3.jpg"></a></li>
+//				</ul>
+//			</div>
+//for(var i = 0 ; i < .xlb li )

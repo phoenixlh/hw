62cc82a4464819301441119fee452b0c231839c9
lihong <1224954159@qq.com>
2018-01-26 23:18:47 +0800

    dfd

--- js/js.js
+++ js/js.js
@@ -1,13 +1,38 @@
-//lunbo
-//<div class="lb">
-//				<ul class="xlb">
-//					<li><a><img src="img/datu1.jpg"></a></li>
-//					<li><a><img src="img/datu2.jpg"></a></li>
-//					<li><a><img src="img/datu3.jpg"></a></li>
-//					<li><a><img src="img/datu4.jpg"></a></li>
-//					<li><a><img src="img/datu1.jpg"></a></li>
-//					<li><a><img src="img/datu2.jpg"></a></li>
-//					<li><a><img src="img/datu3.jpg"></a></li>
-//				</ul>
-//			</div>
-//for(var i = 0 ; i < .xlb li )
+//选项卡
+window.onload = function(){
+	$(".cb").each(function(){
+	
+	}).mouseenter(function(){
+		$(this).find("#ty").addClass("active")
+			   .end()
+			   .find("#cebian").show()
+	}).mouseleave(function(){
+		$(this).find("#ty").removeClass("active")
+		 	   .end()
+			   .find("#cebian").hide()
+	})
+}
+
+
+//大轮播图
+var index = 0 ;
+var timer = null;
+timer = setInterval(autoplay,2000);
+function autoplay(){
+	index++;
+	if(index == $(".ool li").size()){
+		index = 0;
+	}
+	$(".ool li").eq(index).addClass("acc")
+	                      .siblings()
+	                      .removeClass("acc")
+	$(".dt li").eq(index).fadeIn(2000)
+						 .siblings()
+						 .fadeOut(2000)
+}
+
+
+//yidonglunbo
+//$(".spl").click(function(){
+//	startMove($(".big-con"),{left:-1200px})
+//})

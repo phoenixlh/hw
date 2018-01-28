57b3bd490ed28f15b2bf67c9e698ae95925aa444
lihong <1224954159@qq.com>
2018-01-28 18:41:29 +0800

    fda

--- js/js.js
+++ js/js.js
@@ -11,9 +11,9 @@ window.onload = function(){
 		 	   .end()
 			   .find("#cebian").hide()
 	})
-}
-
-
+	
+	
+	
 //大轮播图
 var index = 0 ;
 var timer = null;
@@ -29,10 +29,77 @@ function autoplay(){
 	$(".dt li").eq(index).fadeIn(2000)
 						 .siblings()
 						 .fadeOut(2000)
-}
+}	
 
 
-//yidonglunbo
-//$(".spl").click(function(){
-//	startMove($(".big-con"),{left:-1200px})
-//})
+
+//注册页面-正则
+var flagPhone = null;
+$(".ss1").blur(function(){
+	var str = $(this).val();
+	var reg = /^1(3|5|7|8|9)\d{9}$/;
+	if(!reg.test(str)){
+		$(this).siblings(".error").css("display","block");
+		flagPhone = false;
+	}else{
+		$(this).html("str");
+		$(this).siblings(".error").css("display","none");
+		flagPhone = true;
+	}
+})
+
+var flagName = null;
+$(".ss2").blur(function(){
+	var str = $(this).val();
+	var reg = /^.{6,10}$/;
+	if(!reg.test(str)){
+		$(this).siblings(".error").css("display","block");
+		flagName = false;
+	}else{
+		$(this).html("str");
+		$(this).siblings(".error").css("display","none");
+		flagName = true;
+	}
+})
+
+
+var flagPwd = null;
+$(".ss3").blur(function(){
+	var str = $(this).val();
+	var reg = /^.{6,}$/;
+	if(!reg.test(str)){
+		$(this).siblings(".error").css("display","block");
+		flagPwd = false;
+	}else{
+		$(this).html("str");
+		$(this).siblings(".error").css("display","none");
+		flagPwd = true;
+	}
+})
+
+
+var flagQpwd = null;
+$(".ss4").blur(function(){
+	var str = $(this).val();
+	if( !( str == $(".ss3").val() ) ){
+		$(this).siblings(".error").css("display","block");
+		flagQpwd = false;
+	}else{
+		$(this).html("str");
+		$(this).siblings(".error").css("display","none");
+		flagQpwd = true;
+	}
+})
+
+$(".reg").click(function(){
+	if(!( flagPhone && flagName && flagPwd && flagQpwd )){
+		return false;
+	}
+})
+
+
+}//window的大括号
+
+
+
+
